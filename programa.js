9b6666938833fed1ae683c1adc1fb28760f3ddd4
Andrés Córdoba <111147338+cordobandres@users.noreply.github.com>
2023-02-18 17:33:13 -0500

    PRIMER UPLOAD

---
+++ programa.js
@@ -0,0 +1,17 @@
+function mostrar_inputs(){
+    let valL=document.getElementById("L").value;
+    let valV=document.getElementById("V").value;
+    let valD=document.getElementById("D").value;
+    let undL=document.getElementById("Longitud").value;
+    let undVd=document.getElementById("V1").value;
+    let undVt=document.getElementById("V2").value;
+    let undD=document.getElementById("Diametro").value;
+
+    alert("La perdida de carga es: ",undL);
+    
+    console.log(valL);
+    console.log(valV);
+    console.log(valD);
+    console.log(undL);
+    console.log(undVd),console.log(undVt);
+    console.log(undD); }
