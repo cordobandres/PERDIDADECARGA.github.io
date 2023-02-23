fb4f668f77017d4a20c3a5f98b8d742d5d15e60b
cordobandres <cordobandres47@gmail.com>
2023-02-23 18:01:09 -0500

    CONVERSIONLISTA

--- programa.js
+++ programa.js
@@ -1,17 +1,38 @@
-function mostrar_inputs(){
-    let valL=document.getElementById("L").value;
-    let valV=document.getElementById("V").value;
-    let valD=document.getElementById("D").value;
-    let undL=document.getElementById("Longitud").value;
-    let undVd=document.getElementById("V1").value;
-    let undVt=document.getElementById("V2").value;
-    let undD=document.getElementById("Diametro").value;
 
-    alert("La perdida de carga es: ",undL);
+function mostrar_inputs(){
+    var valL=document.getElementById("L").value;
+    var valV=document.getElementById("V").value;
+    var valD=document.getElementById("D").value;
+    var undL=document.getElementById("Longitud").value;
+    var undVd=document.getElementById("V1").value;
+    var undVt=document.getElementById("V2").value;
+    var undD=document.getElementById("Diametro").value;
     
-    console.log(valL);
-    console.log(valV);
-    console.log(valD);
-    console.log(undL);
-    console.log(undVd),console.log(undVt);
-    console.log(undD); }
+    const unidadesDemedida=[undL,undVd, undD]
+    const valoresDemedida=[valL,valV,valD]
+
+    //CONVERSIÓN UNIDADES//
+    for(i=0; i< unidadesDemedida.length;i++){
+        if (unidadesDemedida[i]==0){
+            valoresDemedida[i]=valoresDemedida[i]*1
+        }
+        else 
+            if(unidadesDemedida[i]==1){ valoresDemedida[i]=valoresDemedida[i]*0.01
+            }
+        else
+            if(unidadesDemedida[i]==2){ valoresDemedida[i]=valoresDemedida[i]*0.001
+            }
+        else
+            if(unidadesDemedida[i]==3){ valoresDemedida[i]=valoresDemedida[i]*0.3048
+            }
+        else
+            if(unidadesDemedida[i]==4){ valoresDemedida[i]=valoresDemedida[i]*0.0254
+            }
+
+    }
+
+    if(undVt==1){
+        valoresDemedida[1]=(valoresDemedida[1])/60
+    }
+    debugger;
+}
