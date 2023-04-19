8c7460913c0cdba97aad2bb841dd2a0aa635d320
cordobandres <cordobandres47@gmail.com>
2023-04-18 23:40:15 -0500

    finn

--- programa.js
+++ programa.js
@@ -7,16 +7,19 @@ function mostrar_inputs(){
     var valQ=document.getElementById("Caudal").value;
     var undQ=document.getElementById("Q").value;
     var undT=document.getElementById("T").value;
-    
+    var ValV=document.getElementById("V").value;
+    var ValR=document.getElementById("R").value;
+    var ValDL=document.getElementById("DL").value;
+
     const unidadesDemedida=[undL, undD]
-    const valoresDemedida=[valL, valD]
+    const valoresDemedida=[valL, valD, ValV, ValR, ValDL]
 
     //CONVERSIÓN UNIDADES//
 
         //CONVERSIÓN LONGITUD Y DIAMETRO//
     for(i=0; i< unidadesDemedida.length;i++){
         if (unidadesDemedida[i]==0){
-            valoresDemedida[i]=valoresDemedida[i]*1
+            valoresDemedida[i]=valoresDemedida[i]*1.0
         }
         else 
             if(unidadesDemedida[i]==1){ valoresDemedida[i]=valoresDemedida[i]*0.01   } 
@@ -28,7 +31,7 @@ function mostrar_inputs(){
             if(unidadesDemedida[i]==4){ valoresDemedida[i]=valoresDemedida[i]*0.0254 }
 
     }
-
+    
     
         //CALCULO DEL AREA//
 
@@ -53,6 +56,53 @@ function mostrar_inputs(){
 
         //CALCULO DE LA VELOCIDAD//
 
-    var Velocidad = valQ/Area
+    var Velocidad = valQ/Area;
+
+        //FUNCIONES//
+
+        function colebrookWhite(Re, D, epsilon) {
+            let f = 0.02;
+            let f_old = f;
+            let error = 1;
+            const maxIterations = 10;
+            let i = 0;
+        
+            while (error > 1 || i < maxIterations) {
+                const A = -2 * Math.log10((epsilon / D) / 3.7 + (2.51 / (Re * Math.sqrt(f))));
+                const B = 1 / Math.sqrt(f);
+                let df = -1 / (2 * Math.sqrt(f) * (Math.log(10) / 2) * (epsilon / D / 3.7 + 2.51 / (Re * Math.sqrt(f))));
+            
+                if (Math.abs(df) < 1e-10) { // verificar si el denominador es cercano a cero
+                df = Math.sign(df) * 1e-10; // establecer la derivada en un valor pequeño pero no nulo
+                }
+            
+                f_old = f;
+                f = f - (A - B) / df;
+                error = Math.abs((f - f_old) / f_old);
+                i++;
+            }
+        
+            return f;
+        }
+    
+        //CALCULO DE REGIMEN LAMINAR//
+
+    var Re = (ValDL*Velocidad*valoresDemedida[1])/ValV
+    var x = (((ValR)/(valoresDemedida[1])/(3.71))+((5.74)/(Math.pow(Re,0.9))))
+    const D = valoresDemedida[1]
+    if (Re>2100){
+        var friccion = (0.25)/(Math.pow(Math.log10(x),2))
+        var perdida1 = friccion*(valoresDemedida[0]/valoresDemedida[1])*((Math.pow(Velocidad,2)/(2*9.81)))
+    }
+    else{
+        const f = colebrookWhite(Re, D, ValR);
+        var perdida1 = f*(valoresDemedida[0]/valoresDemedida[1])*((Math.pow(Velocidad,2)/(2*9.81)))
+    console.log(`El factor de fricción es: ${f}`);
+    console.log(`La perdida de carga es: ${perdida1}`);    
+    }
     debugger;
+
+
+
 }
+
